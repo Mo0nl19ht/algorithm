e2c2c8304178f65a571ed81038d3db487094a673
Mo0nl19ht <mool2480@gmail.com>
2021-11-12 12:47:18 +0900

    11399 정답

--- baekjoon/정렬/11399.py
+++ baekjoon/정렬/11399.py
@@ -4,21 +4,17 @@ n = int(input())
 people = range(1, n+1)
 time = list(map(int, input().split()))
 
-perm = list(permutations(people, n))
-min_cost = 0
-for _, p in enumerate(perm):
-    p = list(p)
-    cost = 0
-    all_cost = 0
-    for i, v in enumerate(p):
-        if i == 0:
-            cost = (time[v-1])
-        else:
-            # tmp.append(tmp[i-1]+time[v-1])
-            cost = cost+time[v-1]
-        all_cost += cost
-    if _ == 0:
-        min_cost = all_cost
-    elif min_cost > all_cost:
-        min_cost = all_cost
-print(min_cost)
+peo_time = {i: v for i, v in enumerate(time)}
+peo_time_sorted = sorted(peo_time.items(), key=lambda x: x[1])
+
+
+cost = 0
+all_cost = 0
+for peo, time in (peo_time_sorted):
+    if cost == 0:
+        cost = (time)
+    else:
+        cost = cost+time
+    all_cost += cost
+
+print(all_cost)
