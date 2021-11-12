4916874da3719baf5a415022417efe8868a7be42
Mo0nl19ht <mool2480@gmail.com>
2021-11-12 12:49:28 +0900

    11399 주석 추가

--- baekjoon/정렬/11399.py
+++ baekjoon/정렬/11399.py
@@ -3,11 +3,12 @@ from itertools import permutations
 n = int(input())
 people = range(1, n+1)
 time = list(map(int, input().split()))
-
+# 사람과 소요시간을 딕셔너리로 관리
 peo_time = {i: v for i, v in enumerate(time)}
 peo_time_sorted = sorted(peo_time.items(), key=lambda x: x[1])
 
-
+# cost는 이전 cost를 포함하므로  계속 새로운 시간소요를 더해준다
+# 전체 시간 소요는 all_cost로 관리
 cost = 0
 all_cost = 0
 for peo, time in (peo_time_sorted):
