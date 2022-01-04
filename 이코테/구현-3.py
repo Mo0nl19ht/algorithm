8a48fced69fab84e76fab0c8e1b5961de64014a1
Mo0nl19ht <mool2480@gmail.com>
2022-01-04 23:16:52 +0900

    구현 3 다시풀기 완료

--- 이코테/구현-3.py
+++ 이코테/구현-3.py
@@ -1,34 +1,35 @@
 n, m = map(int, input().split())
 x, y, head = map(int, input().split())
-x -= 1
-y -= 1
 
-cnt = 0
+
+cnt = 1
 chk = [[0 for k in range(m)] for i in range(n)]
-d_ck = 0
-go = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+head_cnt=0
+go = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 travel = [list(map(int, input().split())) for i in range(n)]
 chk[x][y] = 1
 while True:
     head -= 1
-    print(x, y)
-
-    if chk[x+go[head][0]][y+go[head][1]] == 1:
-        d_ck += 1
+    print(x, y,head,head_cnt)
+    if chk[x+go[head][0]][y+go[head][1]] == 0 and travel[x+go[head][0]][y+go[head][1]]==0:
+        x+=go[head][0]
+        y+=go[head][1]
+        chk[x][y]=1
+        cnt+=1
+        head_cnt=0
         if (head < 0):
             head = 3
         continue
-    x += go[head][0]
-    y += go[head][1]
-    chk[x][y] = 1
-    cnt += 1
-    if (head < 0):
-        head = 3
-
+    else:
+        if (head < 0):
+            head = 3
+        head_cnt+=1
+    if head_cnt==4:
+        head+=1
+        x-=go[head][0]
+        y-=go[head][1]
+        if travel[x][y]==1:
+            break
+        cnt+=1
+        head_cnt=0
 print(cnt)
-
-
-# travel[x+go[0][0]][y+go[0][1]] == 1 and
-#             travel[x+go[1][0]][y+go[1][1]] == 1 and
-#             travel[x+go[2][0]][y+go[2][1]] == 1 and
-#             travel[x+go[3][0]][y+go[3][1]] == 1
