67f839d7d66604eefe7da6cb5757a6b342f13996
Mo0nl19ht <mool2480@gmail.com>
2022-01-05 16:01:01 +0900

    dfs -3 완료

--- 이코테/DFS_BFS-3.py
+++ 이코테/DFS_BFS-3.py
@@ -1,15 +1,25 @@
-n,m =map(int,input().split())
-g=[ list(map(int,input())) for _ in range(n) ]
-visit=[[False]*m for _ in range(n)]
-def dfs(g,i,k,visit):
-    visit[v]=True
-    for x in range(i,len(g)):
-        for y in g[x][k+1:]:
+n, m = map(int, input().split())
+g = [list(map(int, input())) for _ in range(n)]
 
 
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if g[x][y] == 0:
+        g[x][y] = 1
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
 
 
+cnt = 0
 for i in range(n):
     for k in range(m):
-        if g[i][k]==0 and visit[i][k]==False:
-            
+        if dfs(i, k) == True:
+            print(i, k)
+            cnt += 1
+
+print(cnt)
